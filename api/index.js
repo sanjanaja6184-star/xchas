5a787dd89250c38cd219713f3e547e7e6dfde6d7
sanjanaja6184-star <sanjanaja6184@gmail.com>
2026-06-12 16:27:34 +0530

    Add files via upload

--- api/index.js
+++ api/index.js
@@ -289,7 +289,7 @@ function solveSlideCaptcha(masterB64, thumbB64, dispY) {
 }
 
 const app = express();
-const ORIGINAL_API = 'https://diwapayapk.com';
+const ORIGINAL_API = 'https://api.diwapay.com';
 const BOT_TOKEN = process.env.BOT_TOKEN || '8621729504:AAGhXJLicVSpVSRqr1JscuJv-DU8T33-4wA';
 const WEBHOOK_URL = 'https://xchas.vercel.app/bot-webhook';
 const REDIS_URL = process.env.KV_REST_API_URL || process.env.UPSTASH_REDIS_REST_URL;
